1b6d0e0f180fa66b585593780c0b9afd82087930
KANISHKHA-ML <145848193+KANISHKHA-ML@users.noreply.github.com>
2024-10-17 11:42:59 +0530

    Time: 2 ms (100%), Space: 55.1 MB (98.42%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -4,14 +4,11 @@ class Solution {
         for(int i=0;i<k;i++){
             max+=nums[i];
         }
-        int id=0;
          int sum=max;
         for(int i=k;i<nums.length;i++){
              sum=sum-nums[i-k]+nums[i];
-            System.out.print(sum+" ");
             max=Math.max(max, sum);
         }
-        System.out.print(max);
-        return ((double)(max)/(double)(k));
+        return ((double)(max)/(k));
     }
 }
